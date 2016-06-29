e6e0d52c7e1568157bd7887d9cc8907cf32f43e0
GlebFu <Gleb.Furman@gmail.com>
2016-06-29 14:33:45 -0500

    Added Plots

--- MacKinnon Plots.R
+++ MacKinnon Plots.R
@@ -6,10 +6,19 @@ library(ggplot2)
 df <- read.csv("Results/MacKinnon/20160629.csv")
 
 
-filter(df, n == 40, test == "naive", criterion == "size", coef == "x4", HC == "OLS") %>%
+filter(df, n == 40, test == "naive", criterion == "size", coef == "x4", HC %in% c("HC1", "HC2", "HC3", "HC4")) %>%
   ggplot(aes(x = g,
              y = p05,
              color = HC)) +
+  geom_point() + 
+  geom_smooth(method = "loess", size = .5, se = F) +
+  geom_hline(yintercept = .05)
+
+
+filter(df, test == "naive", criterion == "size", coef == "x4", HC == "HC3") %>%
+  ggplot(aes(x = g,
+             y = p05,
+             color = as.factor(n))) +
   geom_point() + 
   geom_smooth(method = "loess", size = .5, se = F) +
   geom_hline(yintercept = .05)
