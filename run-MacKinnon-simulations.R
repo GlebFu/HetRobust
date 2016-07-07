e27c0f098c2b2b3f414076fa40103001e4323d94
James E. Pustejovsky <jepusto@gmail.com>
2016-07-07 16:32:35 -0500

    Updated MacKinnon simulations.

---
+++ run-MacKinnon-simulations.R
@@ -0,0 +1,66 @@
+library(Pusto)
+rm(list=ls())
+source("t-test-and-simulation-functions.R")
+
+MacKinnon_dgm <- function(n = 25, B = c(1, 1, 1, 1, 0, 0), whichX = c(T, T ,T ,T ,T ,F), g = 0, zg = 1) {
+
+  trueB <- B
+  B <- B[whichX]
+  p <- sum(whichX) - 1
+  # Distributions used in generating data
+  X <- cbind(1, matrix(exp(rnorm(n * p)), n, p))
+  colnames(X) <- paste("x", 0:p, sep = "")
+  eta <- rnorm(n)
+  
+  XB <- as.vector(X %*% B)
+  sigma <- zg * XB^g
+  
+  # Generate DV
+  Y <- XB + eta * sigma
+  
+  estimate_model(Y, X, trueB, whichX)
+}
+
+# model <- MacKinnon_dgm()
+# HC <- "HC2"
+# alphas <- c(.005, .01, .05, .10)
+# estimate(HC, model, alphas)
+# 
+# test_sims <- runSim(dgm = MacKinnon_dgm, 
+#                      iterations = 100, n = 25, 
+#                      B = "1 1 1 1 0", whichX = "T T T T T", 
+#                      HC = "HC0 HC1 HC2 HC3 HC4 HC4m HC5", 
+#                      alpha_string = ".005 .01 .05 .10", 
+#                      g = 0, zg = 1)
+# test_sims
+
+#-----------------------------
+# Run MacKinnon Simulation
+#-----------------------------
+set.seed(20160707)
+
+load("scale.rdata")
+design <- list(n = seq(20,100,20),
+               B = "1 1 1 1 0",
+               whichX = "T T T T T",
+               g = seq(0, 2, 0.2),
+               HC = "HC0 HC1 HC2 HC3 HC4 HC4m HC5",
+               alpha_string = ".005 .010 .050 .100")
+
+params <- expand.grid(design, stringsAsFactors = FALSE)
+
+params <- merge(params, scale)
+
+params$iterations <- 20000
+params$seed <- round(runif(1) * 2^30) + 1:nrow(params)
+
+source_obj <- ls()
+cluster <- start_parallel(source_obj = source_obj)
+
+system.time(results <- plyr::mdply(params, .fun = runSim, 
+                                   dgm = MacKinnon_dgm,
+                                   .parallel = TRUE))
+
+stopCluster(cluster)
+
+write.csv(results, file = "Results/MacKinnon/20160707.csv")
---
+++ t-test-and-simulation-functions.R
@@ -0,0 +1,285 @@
+
+#-----------------------------------
+# p-value from a two-sided t-test 
+#-----------------------------------
+
+t_test <- function(t_stats, df) {
+  2 * pt(abs(t_stats), df = df, lower.tail = FALSE)
+}
+
+#-----------------------------------
+# Satterthwaite degrees of freedom 
+#-----------------------------------
+
+Satterthwaite_empirical <- function(sd, X_M, omega, e, H, n) {
+  
+  sigma_hat <- tcrossprod(omega * e^2) / (2 * tcrossprod(omega) * H^2 + 1)
+  diag(sigma_hat) <- (omega^2) * (e^4) / 3
+  I_H <- diag(n) - H
+  
+  A_vec <- omega * X_M^2
+  
+  V_V <- function(a) {
+    B <- I_H %*% (a * I_H)
+    sum(as.vector(t(B)) * as.vector(B * sigma_hat))
+  }
+  den <- apply(A_vec, 2, V_V)
+  
+  return(sd^4 / den)
+}
+
+Satterthwaite_model <- function(X_M, omega, H, h) {
+  
+  df_A <- function(g) {
+    x <- (1 - h) * omega * g^2
+    B <- H^2 * tcrossprod(omega * g^2)
+    diag(B) <- x^2
+    sum(x)^2 / sum(B)
+  }
+  
+  apply(X_M, 2, df_A)
+}
+
+#-----------------------------------
+#Saddlepoint approximation
+#-----------------------------------
+
+saddlepoint_pval <- function(t, Q, d) {
+  
+  eig <- pmax(0, eigen(Q, symmetric = TRUE, only.values=TRUE)$values[1:d])
+  g <- c(1, -t^2 * eig / sum(eig))
+  
+  s_eq <- function(s) sum(g / (1 - 2 * g * s))
+  s_range <- if (s_eq(0) > 0) c(1 / (2 * min(g)), 0) else c(0, 1 / (2 * max(g)))
+  s <- uniroot(s_eq, s_range)$root
+  
+  if (abs(s) > .01) {
+    r <- sign(s) * sqrt(sum(log(1 - 2 * g * s)))
+    q <- s * sqrt(2 * sum(g^2 / (1 - 2 * g * s)^2))
+    p_val <- 1 - pnorm(r) - dnorm(r) * (1 / r - 1 / q)
+  } else {
+    p_val <- 0.5 - sum(g^3) / (3 * sqrt(pi) * sum(g^2)^(3/2))
+  }
+  
+  return(p_val)
+}
+
+saddle <- function(t_stats, X_M, omega, e, H, n, p, approx = "model") {
+  if (!all(is.finite(t_stats))) {
+    return(rep(1,length(t_stats)))
+  } else {
+    I_H <- diag(n) - H
+    A_vec <- omega * X_M^2
+    if (approx == "model") {
+      Qs <- apply(A_vec, 2, function(x) I_H %*% (x * I_H))
+    } else {
+      Qs <- apply(A_vec, 2, function(x) (e^2 * I_H) %*% (x * I_H))
+    }
+    Qs <- lapply(data.frame(Qs), matrix, nrow = n, ncol = n)
+    p_vals <- mapply(saddlepoint_pval, t = t_stats, Q = Qs, d = n - p)
+    return(p_vals)
+  }
+}
+
+
+#------------------------------------------------
+# Kauermann-Carroll Edgeworth approximations
+#------------------------------------------------
+
+KC_pval <- function(t_stats, df) {
+  2 * (1 - pnorm(abs(t_stats))) + (abs(t_stats)^3 + abs(t_stats)) * dnorm(t_stats) / (2 * df)
+}
+
+KC_CI <- function(t_stats, df, M, n, p, alphas) {
+  z_crit <- qnorm(1 - alphas / 2)
+  t_crit <- qt(1 - alphas / 2, df = n - p)
+  
+  test_t <- function(t, df, m) {
+    z_alpha <- t_crit + (z_crit^3 + z_crit) * (1 / df - m^2 / (n - p)) / 4
+    min(c(alphas[abs(t) > z_alpha],1))
+  }
+  
+  mapply(test_t, t = t_stats, df = df, m = diag(M))
+}
+
+#-------------------------------------------
+# Rothenberg Edgeworth approximations
+#-------------------------------------------
+
+Rothenberg_pvals <- function(t_stats, sd, X_M, n, H, h, omega, e, df, alphas, approx = "model") {
+  
+  X_M_sq <- X_M^2
+  
+  if (approx=="model") {
+    a_vec <- 0
+    b_vec <- -colSums(omega * h * X_M_sq) / colSums(X_M_sq)
+  } else {
+    I_H <- diag(n) - H
+    sigma <- e^2
+    q_ii <- colSums(I_H^2 * sigma) - sigma
+    f_i <- I_H %*% (sigma * X_M)
+    a_vec <- colSums(omega * f_i^2 * X_M_sq) / sd^4
+    b_vec <- colSums(omega * q_ii * X_M_sq) / sd^2
+  }
+  
+  p_val <- 2 * (1 - pnorm(abs(t_stats) * pmax(0, 2 - (1 + t_stats^2) / (2 * df) + a_vec * (t_stats^2 - 1) + b_vec) / 2))
+  p_val <- pmin(1, p_val)
+  
+  z_crit <- qnorm(1 - alphas / 2)
+  test_t <- function(t, df, a, b) {
+    z_alpha <- z_crit + (z_crit^3 + z_crit) / (4 * df) - z_crit * (a * (z_crit^2 - 1) + b) / 2
+    min(c(alphas[abs(t) > z_alpha],1))
+  }
+  CI <- mapply(test_t, t = t_stats, df = df, a = a_vec, b = b_vec)
+  
+  data.frame(p_val, CI)
+}
+
+#-----------------------------------
+# testing function
+#-----------------------------------
+
+estimate <- function(HC, model, alphas) {
+
+  omega <- with(model, switch(HC,
+                  HC0 = rep(1,n),
+                  HC1 = rep(n / (n - p), n),
+                  HC2 = 1 / (1 - h),
+                  HC3 = (1 - h)^(-2),
+                  HC4 = (1 - h)^(-pmin(h * n / p, 4)),
+                  HC4m = (1 - h)^(-(pmin(h * n / p, 1) + pmin(h * n / p, 1.5))),
+                  HC5 = (1 - h)^(-pmin(h * n / p, pmax(4, .7 * n * max(h) / p)) / 2)
+                  ))
+  
+  sd <- with(model, sqrt(colSums(omega * (X_M * e)^2)))
+  
+  coefs_to_test <- with(model, c(coefs - B, coefs))
+  t_stats <- coefs_to_test / sd
+  
+  # testing 
+  pValues <- data.frame(HC = HC, 
+                        coef = rep(colnames(model$X), 2), 
+                        criterion = rep(c("size","power"), each = model$p))
+  
+  # naive t tests
+  pValues$naive <- t_test(t_stats = t_stats, df = model$n - model$p)
+  
+  # Satterthwaite tests
+  df_E <- Satterthwaite_empirical(sd = sd, model$X_M, omega, model$e, model$H, model$n)
+  df_H <- Satterthwaite_model(model$X_M, omega, model$H, model$h)
+  
+  pValues$Satt_E <- t_test(t_stats = t_stats, df = df_E)
+  pValues$Satt_H <- t_test(t_stats = t_stats, df = df_H)
+
+  # Kauermann-Carroll edgeworth approximations
+  pValues$KCp_E <- KC_pval(t_stats = t_stats, df = df_E)
+  pValues$KCp_H <- KC_pval(t_stats = t_stats, df = df_H)
+  pValues$KCCI_E <- KC_CI(t_stats = t_stats, df = df_E, 
+                          M = model$M, n = model$n, p = model$p, alphas = alphas)
+  pValues$KCCI_H <- KC_CI(t_stats = t_stats, df = df_H, 
+                          M = model$M, n = model$n, p = model$p, alphas = alphas)
+  
+  # Rothenberg's edgeworth approximations
+  Roth_E <- Rothenberg_pvals(t_stats, sd, model$X_M, model$n, 
+                                  model$H, model$h, omega, model$e, 
+                                  df = df_E, alphas = alphas, approx = "empirical")
+  pValues$Rp_E <- Roth_E$p_val
+  pValues$RCI_E <- Roth_E$CI
+  Roth_H <- Rothenberg_pvals(t_stats, sd, model$X_M, model$n, 
+                                  model$H, model$h, omega, model$e, 
+                                  df = df_H, alphas = alphas, approx = "model")
+  pValues$Rp_H <- Roth_H$p_val
+  pValues$RCI_H <- Roth_H$CI
+  
+  # saddlepoint approximations
+  pValues$saddle_E <- saddle(t_stats = t_stats, X_M = model$X_M, 
+                             omega = omega, e = model$e,
+                             H = model$H, n = model$n, 
+                             p = model$p, approx = "empirical")
+  pValues$saddle_H <- saddle(t_stats = t_stats, X_M = model$X_M, 
+                             omega = omega, e = model$e, 
+                             H = model$H, n = model$n, 
+                             p = model$p, approx = "model")
+
+  # round(pValues[1:5,-(1:3)],4)
+  # round(pValues[6:10,-(1:3)],4)
+  return(pValues)
+  
+}
+
+#-----------------------------
+# Fit regression model
+#-----------------------------
+
+estimate_model <- function(Y, X, trueB, whichX) {
+  
+  #X <- X[, whichX]
+  B <- trueB[whichX]
+  
+  n <- nrow(X)
+  p <- ncol(X)
+  
+  M <- chol2inv(chol(t(X) %*% X))
+  X_M <- X %*% M
+  coefs <- colSums(Y * X_M)
+  e <- Y - as.vector(X %*% coefs)
+  
+  H <- X_M %*% t(X)
+  h <- diag(H)
+  
+  values <- list(X = X, Y = Y, B = B, X_M = X_M, H = H, h = h, e = e, coefs = coefs, n = n, p = p, M = M)
+  
+  return(values)
+}
+
+#-----------------------------------
+# calculate rejection rates
+#-----------------------------------
+
+reject_rates <- function(p_vals, alphas) {
+  rejects <- lapply(alphas, function(a) ifelse(is.na(p_vals), FALSE, p_vals <= a))
+  data.frame(percent_NA = mean(is.na(p_vals)), 
+             lapply(rejects, mean))
+}
+
+#-----------------------------------
+# simulation driver
+#-----------------------------------
+
+# iterations <- 20
+# n <- 25
+# B <- "1 1 1 1 0"
+# whichX <- "T T T T T"
+# g <- 0
+# zg <- 1
+# HC <- "HC0 HC1 HC2 HC3 HC4 HC4m HC5"
+# alpha_string <- ".005 .01 .05 .10"
+# seed <- NULL
+
+runSim <- function(dgm, iterations, n, B, whichX, HC, alpha_string, seed = NULL,...) {
+  require(tidyr)
+  require(dplyr)
+  if (!requireNamespace("plyr", quietly = TRUE)) {
+    stop("The plyr package must be installed")
+  }
+  
+  B <- as.numeric(unlist(strsplit(B, " ")))
+  whichX <- as.logical(unlist(strsplit(whichX, " ")))
+  HC <- as.character(unlist(strsplit(HC, " ")))
+  alphas <- as.numeric(unlist(strsplit(alpha_string, " ")))
+  names(alphas) <- paste0("p",as.character(unlist(strsplit(alpha_string, " "))))
+  
+  if (!is.null(seed)) set.seed(seed)
+  
+  reps <- plyr::rdply(iterations, {
+    model <- dgm(n = n, B = B, whichX = whichX, ...)
+    plyr::ldply(HC, estimate, model = model, alphas = alphas)
+  })
+  
+  # performance calculations
+  reps %>% 
+    select(-1) %>%
+    gather("test","pval", ends_with("_E"), ends_with("_H")) %>%
+    group_by(HC, coef, criterion, test) %>%
+    do(reject_rates(.$pval, alphas = alphas))
+}
