49a038293927039da94db2357704a94a8a95b37e
jepusto <jepusto@gmail.com>
2017-04-17 15:42:00 -0500

    Relative power results.

--- New simulations/analyze one-dim sims - power results.R
+++ New simulations/analyze one-dim sims - power results.R
@@ -20,18 +20,170 @@ power_results %>%
   select(starts_with("p0."), starts_with("n0.")) %>%
   summary()
 
-power_results %>%
-  filter(n == 25, e_dist == "norm") %>%
-  ggplot(aes(beta, p0.01, color = test)) + 
-  geom_line() + 
-  facet_grid(z ~ x_skew, scales = "free_y", labeller = "label_both") + 
-  expand_limits(y = 0) + 
-  theme_light() + 
-  theme(legend.position = "bottom", 
-        strip.text.x = element_text(color = "black"), 
-        strip.text.y = element_text(color = 'black')) + 
-  labs(x = quote(beta), y = "Rejection rate", 
-       color = "", linetype = "", shape = "") + 
-  guides(color = guide_legend(nrow = 1))
+rel_power_results <-
+  power_results %>%
+  select(-HC, -seed, -iterations, -span, -coef, -percent_NA) %>%
+  gather("level","rate", starts_with("p0"), starts_with("n0"))
+
+rel_power_results <- 
+  rel_power_results %>%
+  filter(test=="naive HC5") %>%
+  select(-test) %>%
+  rename(HC5_rate = rate) %>%
+  inner_join(rel_power_results) %>%
+  filter(test != "naive HC5") %>%
+  mutate(rel_power = rate / HC5_rate)
 
 
+rel_power_plot <- function(dat) {
+  ggplot(dat, aes(beta, rel_power, color = test)) + 
+    geom_line(size = 1) + 
+    # geom_smooth(method = "loess", se = FALSE, span = 0.2) + 
+    facet_grid(z ~ x_skew, scales = "free_y", labeller = "label_both") + 
+    theme_light() + 
+    theme(legend.position = "bottom", 
+          strip.text.x = element_text(color = "black"), 
+          strip.text.y = element_text(color = 'black')) + 
+    labs(x = quote(beta), y = "Relative power", 
+         color = "", linetype = "", shape = "") + 
+    guides(color = guide_legend(nrow = 1))
+}
+
+#-----------------------------------
+# alpha = .05
+#-----------------------------------
+
+# normal errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="norm", level=="p0.05") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="norm", level=="p0.05") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="norm", level=="p0.05") %>%
+  rel_power_plot()
+
+# chi-squared errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="chisq", level=="p0.05") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="chisq", level=="p0.05") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="chisq", level=="p0.05") %>%
+  rel_power_plot()
+
+# t(5) errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="t5", level=="p0.05") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="t5", level=="p0.05") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="t5", level=="p0.05") %>%
+  rel_power_plot()
+
+#-----------------------------------
+# alpha = .01
+#-----------------------------------
+
+# normal errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="norm", level=="p0.01") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="norm", level=="p0.01") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="norm", level=="p0.01") %>%
+  rel_power_plot()
+
+# chi-squared errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="chisq", level=="p0.01") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="chisq", level=="p0.01") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="chisq", level=="p0.01") %>%
+  rel_power_plot()
+
+# t(5) errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="t5", level=="p0.01") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="t5", level=="p0.01") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="t5", level=="p0.01") %>%
+  rel_power_plot()
+
+
+#-----------------------------------
+# alpha = .005
+#-----------------------------------
+
+# normal errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="norm", level=="p0.005") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="norm", level=="p0.005") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="norm", level=="p0.005") %>%
+  rel_power_plot()
+
+# chi-squared errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="chisq", level=="p0.005") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="chisq", level=="p0.005") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="chisq", level=="p0.005") %>%
+  rel_power_plot()
+
+# t(5) errors
+
+rel_power_results %>%
+  filter(n==25, e_dist=="t5", level=="p0.005") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==50, e_dist=="t5", level=="p0.005") %>%
+  rel_power_plot()
+
+rel_power_results %>%
+  filter(n==100, e_dist=="t5", level=="p0.005") %>%
+  rel_power_plot()
